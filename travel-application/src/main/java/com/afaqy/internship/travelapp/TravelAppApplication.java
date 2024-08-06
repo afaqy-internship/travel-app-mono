4a201afd2375f0b99be64111337fdf1b8d889292
mona-adel <mona.adel@afaqy.com>
2024-08-06 16:19:54 +0300

    monolithic demo

--- travel-application/src/main/java/com/afaqy/internship/travelapp/TravelAppApplication.java
+++ travel-application/src/main/java/com/afaqy/internship/travelapp/TravelAppApplication.java
@@ -3,7 +3,7 @@ package com.afaqy.internship.travelapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages  = {"com.murilohenzo"})
+@SpringBootApplication(scanBasePackages  = {"com.afaqy.internship"})
 public class TravelAppApplication {
 
 	public static void main(String[] args) {
